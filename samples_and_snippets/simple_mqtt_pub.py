7928790cb7d6d370084477aa1d55cbdaa3798f8a
Capeeslash <kenyoti.p@gmail.com>
2023-06-19 01:02:57 +0800

    MQTT Test server New Published (Geese) - Created Use of json - completed Connection - tested

---
+++ samples_and_snippets/simple_mqtt_pub.py
@@ -0,0 +1,7 @@
+import paho.mqtt.client as paho
+
+BROKER, PORT = "127.0.0.1", 1883
+
+client = paho.Client()
+client.connect(BROKER, PORT)
+client.publish("lot/sensor", "Hello from Python")
---
+++ samples_and_snippets/simple_mqtt_sub.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as paho
+from paho.mqtt.client import MQTTMessage
+
+BROKER, PORT = "127.0.0.1", 1883
+
+
+def on_message_received(client, userdata, msg):
+    print(f'Received {msg.payload.decode()}')
+
+
+client = paho.Client()
+client.on_message = on_message_received
+client.connect(BROKER, PORT)
+client.subscribe("carpark/MOO/parking-lot/controller")
+client.loop_forever()
